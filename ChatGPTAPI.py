5c7b7625edaf52a573eb0a957efe7b40c6e41264
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-03-19 16:57:41 +0530

    Update ChatGPTAPI.py

--- ChatGPTAPI.py
+++ ChatGPTAPI.py
@@ -1,7 +1,12 @@
+#Importing openAI Library into the runtime
 import openai
-openai.api_key="AUTHORISATION_KEY"
+#Setting the API AUTHORISATION_KEY
+openai.api_key="AUTHORIZATION_KEY" #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
+#Defining model engine
 model_engine="text-davinci-002"
+#An infinite Loop for successful chat
 while True:
         prompt=str(input("ENTER YOUR PROMPT :"))
-        completion=openai.Completion.create(engine=model_engine,prompt=prompt,max_tokens=1024,n=1,stop=None,temperature>        response=completion.choices[0].text
+        completion=openai.Completion.create(engine=model_engine,prompt=prompt,max_tokens=1024,n=1,stop=None,temperature=0.9)
+        response=completion.choices[0].text
         print(response)
